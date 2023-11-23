39536fccf8ed04f2b686ea415a15062662c670dd
RamideMuradova <ramidemuradova003@gmail.com>
2023-11-23 05:21:44 +0400

    ./04.js-dom.practise/

---
+++ 04.js-dom.practise/main.js
@@ -0,0 +1,36 @@
+let inputs = document.querySelector(".inputs");
+let numberOne = document.querySelector(".number1");
+let numberTwo = document.querySelector(".numberTwo");
+let addition = document.querySelector(".addition");
+let substract = document.querySelector(".substract");
+let multiply = document.querySelector(".multiply");
+let division = document.querySelector(".division");
+let reset = document.querySelector(".reset");
+
+let count = 0;
+
+addition.addEventListener("click", function () {
+  count += +numberOne.value + +numberTwo.value;
+  inputs.innerText = count;
+});
+
+substract.addEventListener("click", function () {
+  count += +numberOne.value - +numberTwo.value;
+  inputs.innerText = count;
+});
+
+multiply.addEventListener("click", function () {
+  count += +numberOne.value * +numberTwo.value;
+  inputs.innerText = count;
+});
+
+division.addEventListener("click", function () {
+  count += +numberOne.value / +numberTwo.value;
+  inputs.innerText = count;
+});
+
+reset.addEventListener("click", function () {
+  numberOne.value = " ";
+  numberTwo.value = " ";
+  inputs.innerText = 0;
+});
