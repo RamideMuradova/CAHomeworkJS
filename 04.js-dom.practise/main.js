ae28e2b258318f9c54d07a11f373f04c64cf404f
RamideMuradova <ramidemuradova003@gmail.com>
2023-11-26 16:11:38 +0400

    ./04.js-dom.practise/

--- 04.js-dom.practise/main.js
+++ 04.js-dom.practise/main.js
@@ -1,5 +1,5 @@
 let inputs = document.querySelector(".inputs");
-let numberOne = document.querySelector(".number1");
+let numberOne = document.querySelector(".numberOne");
 let numberTwo = document.querySelector(".numberTwo");
 let addition = document.querySelector(".addition");
 let substract = document.querySelector(".substract");
@@ -10,27 +10,27 @@ let reset = document.querySelector(".reset");
 let count = 0;
 
 addition.addEventListener("click", function () {
-  count += +numberOne.value + +numberTwo.value;
+  count = +numberOne.value + +numberTwo.value;
   inputs.innerText = count;
 });
 
 substract.addEventListener("click", function () {
-  count += +numberOne.value - +numberTwo.value;
+  count = +numberOne.value - +numberTwo.value;
   inputs.innerText = count;
 });
 
 multiply.addEventListener("click", function () {
-  count += +numberOne.value * +numberTwo.value;
+  count = +numberOne.value * +numberTwo.value;
   inputs.innerText = count;
 });
 
 division.addEventListener("click", function () {
-  count += +numberOne.value / +numberTwo.value;
+  count = +numberOne.value / +numberTwo.value;
   inputs.innerText = count;
 });
 
 reset.addEventListener("click", function () {
-  numberOne.value = " ";
-  numberTwo.value = " ";
+  numberOne.value = "";
+  numberTwo.value = "";
   inputs.innerText = 0;
 });
